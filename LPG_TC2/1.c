a5fa11e9ea84d72d1e1d1c5ba51e54e9c6d34b99
eberjoe <eberjoe@gmail.com>
2019-04-06 22:30:53 -0300

    Add exercício 1

---
+++ LPG_TC2/1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, v=0;
+    char frase[100], c;
+    printf("Digite uma string de ate 100 caracteres: ");
+    gets(frase);
+    for (i=0; i<=strlen(frase); i++) {
+        c=frase[i];
+        if (c=='a' || c=='e' || c=='i' || c=='o' || c=='u') {
+            v++;
+        }
+    }
+    printf("%s tem %d vogais", frase, v);
+
+    return 0;
+}
