612842d3c41df1097f5bb54d154f073f9216a6af
Eber Rodrigues <eberjoe@gmail.com>
2019-05-13 20:37:20 -0300

    Adicionado 2

---
+++ LPG_TC3/2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+    char c;
+    int i=0;
+    char *p;
+    do {
+        printf("Entre o caractere %d"; i);
+        scanf("%c", &c);
+    } while c!="="
+    printf("%s", c);
+    return 0;
+}
