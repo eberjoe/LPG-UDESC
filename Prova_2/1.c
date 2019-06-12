221815fd2ce1689ad34275fe57b6481b358619be
Eber Martins Rodrigues <eber.rodrigues@neogrid.com>
2019-06-12 13:31:51 -0300

    Add qestão 1 da prova 2

---
+++ Prova_2/1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int *vetor=NULL, i=0, e, soma=0;
+    do {
+        printf("Entre um numero inteiro, ou '0' para sair: ");
+        scanf("%d", &e);
+        while (getchar() != '\n'); // consome o \n em excesso no buffer
+        vetor=(int *) realloc(vetor, sizeof(int)*(i+1));
+        vetor[i]=e;
+        i++;
+    } while (e);
+    for (e=0; e<i; e++) {
+        printf("%d ", vetor[e]);
+        soma+=vetor[e];
+    }
+    printf("\n%d", soma);
+    return 0;
+}
